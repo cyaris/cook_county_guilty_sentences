d14ef649a68a7d798dfa2bcf1be34dafb3de04b0
cyaris <charlieyaris@gmail.com>
2018-10-29 07:36:28 -0500

    Made updated to cook county classification project.

--- my_functions.py
+++ my_functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37fd22d64f06bdce87557bfbb2d8d41326cf332bd92452ced19ded7a3cdb1ad5
-size 6092
+oid sha256:a677f0f61955ebfb3d5d7d35992e90142f33686cc281c5128e09338100bad7ea
+size 6088
